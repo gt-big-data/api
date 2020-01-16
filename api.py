90a7dbde2daef06f6383856d5c7ddb18293f948f
davonprewitt <davonprewitt@gmail.com>
2020-01-15 20:41:14 -0500

    Adds streets/ and streets/<int:id_1>:<int:id_2> endpoints

--- api.py
+++ api.py
@@ -39,5 +39,37 @@ def get_intersections_by_id(id):
         return "405: Restricted method"
 
 
+@app.route("/streets", methods=['GET'])
+def get_streets():
+    table_id = table_id_base.format("streets")
+    table = client.get_table(table_id)
+    QUERY = (
+        'SELECT * '
+        'FROM `{}` ').format(table_id)
+    query_job = client.query(QUERY)
+    rows = [dict(row) for row in query_job.result()]
+    if request.method == 'GET':
+        return jsonify(rows)
+    else:
+        return "405: Restricted method"
+
+
+@app.route("/streets/<int:id_1>:<int:id_2>", methods=['GET'])
+def get_streets_by_id(id_1, id_2):
+    table_id = table_id_base.format("streets")
+    table = client.get_table(table_id)
+    QUERY = (
+        'SELECT * '
+        'FROM `{}` '
+        'WHERE node_id_start={} '
+        'AND node_id_end={}').format(table_id, id_1, id_2)
+    query_job = client.query(QUERY)
+    rows = [dict(row) for row in query_job.result()]
+    if request.method == 'GET':
+        return jsonify(rows[0])
+    else:
+        return "405: Restricted method"
+
+
 if __name__ == '__main__':
     app.run()
