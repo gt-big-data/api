b39e717ab0df2b6a385e4335733ff8c5ddaaf6fe
Rahul <rchawla36@gatech.edu>
2020-03-02 19:01:56 -0500

    Added /path route for finding paths between nodes

--- api.py
+++ api.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, jsonify
 from google.cloud import bigquery
 import requests
-
+import networkx as nx
+import json
 
 app = Flask(__name__)
 
@@ -63,6 +64,57 @@ def get_streets():
         return jsonify(rows)
     else:
         return "405: Restricted method"
+
+@app.route("/path/<int:id_1>:<int:id_2>", methods=['GET'])
+def path(id_1, id_2):
+    table_id = table_id_base.format("streets")
+    table = client.get_table(table_id)
+    QUERY = (
+        'SELECT * '
+        'FROM `{}` ').format(table_id)
+    query_job = client.query(QUERY)
+    rows = [dict(row) for row in query_job.result()]
+    if request.method == 'GET':
+        
+        graph = {}
+        table =  jsonify(rows)
+        G = nx.Graph()
+
+
+        for object in rows:
+            if object["node_id_start"] not in graph:
+                
+                AdjNode = (object["node_id_end"], object["true_duration_s"])
+                val = [AdjNode]
+                graph[object["node_id_start"]] = val
+                G.add_edge(object["node_id_start"], object["node_id_end"], weight=object["true_duration_s"])
+
+
+            curAdjNode = (object["node_id_end"], object["true_duration_s"])
+            graph[object["node_id_start"]].append(curAdjNode)
+            G.add_edge(object["node_id_start"], object["node_id_end"], weight=object["true_duration_s"])
+
+
+
+        if id_1 not in graph or id_2 not in graph:
+            return "<h3>Invalid Source or Destination</h3>"
+
+        if nx.has_path(G, id_1, id_2):
+            output = "<h3>The shortest path from " + str(id_1) + " to " + str(id_2) + " is:</h3> <br />"
+            output += str(nx.shortest_path(G, id_1, id_2, weight='weight')) + " <br />"
+            output += "<h3>Cost (Seconds):</h3>" + str(nx.shortest_path_length(G, id_1, id_2, weight='weight'))
+
+        else:
+            output = "<h3>No path exists :("
+        
+        
+
+        return output
+
+
+    else:
+        return "405: Restricted method"
+
 
 
 @app.route("/streets/<int:id_1>:<int:id_2>", methods=['GET'])
