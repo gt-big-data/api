6ee33d671f6262911fa48e220a792cf243dbf70f
W Touchstone <wtouchstone3@gmail.com>
2020-03-02 19:00:35 -0500

    script for adding data to database, not complete

--- tools/populatetable.py
+++ tools/populatetable.py
@@ -1,5 +1,6 @@
 from google.cloud import bigquery
 from flask import Flask, request, jsonify
+import time as time
 
 client = bigquery.Client()
 table_id_base = "bdc-backend.city_simulator.{}"
@@ -7,64 +8,24 @@ table_id_base = "bdc-backend.city_simulator.{}"
 table_id = table_id_base.format("census_shape_points")
 table = client.get_table(table_id)
 f = open("points_table.csv")
-f.readline()
+print(f.readline())
 
 rows_to_insert = list()
-##### census shape points
-# point id: integer
-# centroid id: integer
 
 
-for line in range(5):
-    i = f.readline()
-    i = i.strip()
-    i = i.split(",")
-    print(i)
 
-
-#errors = client.insert_rows(table, rows_to_insert)  # Make an API request.
-#if errors == []:
-#    print("New rows have been added.")
-
-#for line in range(9999):
-#    i = f.readline()
-#    i = i.strip()
-#    i = i.split(",")
-#    if (i != []):
-#        rows_to_insert.append((i[1].strip('"'), float(i[3]), float(i[4]), 0))
-
-
-#errors = client.insert_rows(table, rows_to_insert)  # Make an API request.
-#if errors == []:
-#    print("New rows have been added.")
-    
-
-#for line in range(9999):
-#    i = f.readline()
-#    i = i.strip()
-#    i = i.split(",")
-#    if (i != []):
-#        rows_to_insert.append((i[1].strip('"'), float(i[3]), float(i[4]), 0))
-
-
-#errors = client.insert_rows(table, rows_to_insert)  # Make an API request.
-#if errors == []:
-#    print("New rows have been added.")
-    
-
-#for line in range(9999):
-#    i = f.readline()
-#    i = i.strip()
-#    i = i.split(",")
-#    if (i != []):
-#        try:
-#            rows_to_insert.append((i[1].strip('"'), float(i[3]), float(i[4]), 0))
-#        except:
-#            pass
-
-
-#errors = client.insert_rows(table, rows_to_insert)  # Make an API request.
-#if errors == []:
-#    print("New rows have been added.")
-    
-
+for i in range(115):
+    rows_to_insert = list()
+    for line in range(9999):
+        i = f.readline()
+        i = i.strip()
+        i = i.split(",")
+        if (i != []):
+            i[2] = int(i[2].strip('"'))
+            i[4] = float(i[4].strip('"'))
+            i[5] = float(i[5].strip('"'))
+        rows_to_insert.append((i[3], i[1], i[4], i[5]))
+    errors = client.insert_rows(table, rows_to_insert)  # Make an API request.
+    if errors == []:
+        print("New rows have been added.")
+    time.sleep(1)
--- tools/test.py
+++ tools/test.py
@@ -4,12 +4,12 @@ from flask import Flask, request, jsonify
 client = bigquery.Client()
 table_id_base = "bdc-backend.city_simulator.{}"
 
-table_id = table_id_base.format("census_centroids")
+table_id = table_id_base.format("census_shape_points")
 table = client.get_table(table_id)
 
 QUERY = (
         'SELECT * '
-        'FROM `{}` where lat < 33.35344').format(table_id)
+        'FROM `{}`').format(table_id)
 query_job = client.query(QUERY)
 
 for row in query_job:
